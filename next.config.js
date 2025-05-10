4ba83e871ba81be2bacf33029155f0915732107a
Ken Wu <wooqianghao@gmail.com>
2025-05-10 20:54:25 +0100

    Add portfolio code with GitHub Pages deployment setup

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ken-wu-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ken-wu-portfolio/' : '',
   trailingSlash: true,
 }
 
